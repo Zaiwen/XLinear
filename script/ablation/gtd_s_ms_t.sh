7aee068471207626c90d43bce3f4c3ebf5dc40af
Xinyang1987 <3102037608@qq.com>
2025-11-16 14:22:00 +0800

    add ablation script

---
+++ script/ablation/gtd_s_ms_t.sh
@@ -0,0 +1,110 @@
+model_name=XLinear-ES
+root_path_name=./dataset/environment
+data_path_name=GTD_S.csv
+model_id_name=gtd_s
+data_name=custom
+random_seed=2025
+
+python -u run_longExp.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id gtd_s_96_96 \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --seq_len 96 \
+    --pred_len 96 \
+    --enc_in 16 \
+    --d_model 512\
+    --t_ff 256\
+    --c_ff 16 \
+    --t_dropout 0\
+    --c_dropout 0\
+    --head_dropout 0\
+    --embed_dropout 0\
+    --patience 3\
+    --des 'Exp' \
+    --train_epochs 10 \
+    --batch_size 32 \
+    --itr 1 \
+    --learning_rate 0.0001
+
+python -u run_longExp.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id gtd_s_96_192 \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --seq_len 96 \
+    --pred_len 192 \
+    --enc_in 16 \
+    --d_model 128\
+    --t_ff 32\
+    --c_ff 16 \
+    --t_dropout 0\
+    --c_dropout 0\
+    --head_dropout 0.2\
+    --embed_dropout 0\
+    --patience 3\
+    --des 'Exp' \
+    --train_epochs 10 \
+    --batch_size 128 \
+    --itr 1 \
+    --learning_rate 0.0002
+
+python -u run_longExp.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id gtd_s_96_336 \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --seq_len 96 \
+    --pred_len 336 \
+    --enc_in 16 \
+    --d_model 64\
+    --t_ff 32\
+    --c_ff 16 \
+    --t_dropout 0.2\
+    --c_dropout 0\
+    --head_dropout 0.3\
+    --embed_dropout 0\
+    --patience 3\
+    --des 'Exp' \
+    --train_epochs 10 \
+    --batch_size 128 \
+    --itr 1 \
+    --learning_rate 0.0002
+
+python -u run_longExp.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id gtd_s_96_720 \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --seq_len 96 \
+    --pred_len 720 \
+    --enc_in 16 \
+    --d_model 64\
+    --t_ff 32\
+    --c_ff 16 \
+    --t_dropout 0.1\
+    --c_dropout 0\
+    --head_dropout 0.3\
+    --embed_dropout 0\
+    --patience 3\
+    --des 'Exp' \
+    --train_epochs 10 \
+    --batch_size 128 \
+    --itr 1 \
+    --learning_rate 0.0003
